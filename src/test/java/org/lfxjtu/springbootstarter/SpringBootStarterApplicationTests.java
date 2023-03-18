f348e7faa655bc2e0e353e9c7f0c9a6a021b936a
Fang Liu <lfxjtu@gmail.com>
2023-03-18 21:02:06 +1300

    initial commit

---
+++ src/test/java/org/lfxjtu/springbootstarter/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lfxjtu.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
